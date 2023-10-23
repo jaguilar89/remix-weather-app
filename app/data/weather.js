7b1ade3a68162f111530b8f754017228d5f49841
Jose Aguilar <jaguilarny@gmail.com>
2023-10-23 17:08:41 -0400

    add utility function for location info, install dayjs for date formatting

--- app/data/weather.js
+++ app/data/weather.js
@@ -1,19 +1,38 @@
+const API_KEY = 'cc2955eaa05b2beceea27a0fc21f4767';
+const LOCATION_ENDPOINT = 'http://api.openweathermap.org/geo/1.0/reverse';
+const DATA_ENDPOINT = 'https://api.openweathermap.org/data/2.5/onecall';
+
 export const getWeatherData = async () => {
-    const API_KEY = 'cc2955eaa05b2beceea27a0fc21f4767';
-    const ENDPOINT = 'https://api.openweathermap.org/data/2.5/onecall';
 
     try {
-        const res = await fetch(`${ENDPOINT}?lat=40.730610&lon=-73.935242&units=imperial&exclude=alerts&appid=${API_KEY}`);
+        const res = await fetch(`${DATA_ENDPOINT}?lat=40.730610&lon=-73.935242&units=imperial&exclude=alerts&appid=${API_KEY}`);
 
         if (!res.ok) {
             throw new Error(`API responded with status: ${res.status}`);
         }
 
-        const data = await res.json();
-        return data;
+        const weatherData = await res.json();
+        return weatherData;
 
     } catch (err) {
-        console.error(err);
+        console.log(err);
     }
 
+}
+
+export const getLocationData = async () => {
+
+    try {
+        const res = await fetch(`${LOCATION_ENDPOINT}?lat=40.730610&lon=-73.935242&limit=5&appid=${API_KEY}`)
+
+        if (!res.ok) {
+            throw new Error(`API responded with status: ${res.status}`);
+        }
+
+        const locationData = await res.json();
+        return locationData;
+
+    } catch (err) {
+        console.log(err)
+    }
 }
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,16 +1,30 @@
-import { getWeatherData } from "../data/weather";
+import { useLoaderData } from "@remix-run/react";
+import { getLocationData, getWeatherData } from "../data/weather";
+import dayjs from "dayjs";
+import LocalizedFormat from 'dayjs/plugin/localizedFormat.js';
 
 export const meta = () => {
   return [{ title: "Remix Weather App" }];
 };
 
 export const loader = async () => {
-  const weatherData = await getWeatherData();
-  console.log(weatherData)
-  return weatherData;
+    const [locationData, weatherData] = await Promise.all([
+        getLocationData(),
+        getWeatherData()
+    ]);
+
+    return {
+        location: locationData[0],
+        weatherData: weatherData
+    }
 }
 
 export default function Index() {
+    const data = useLoaderData();
+    console.log(data.weatherData.current.weather[0])
+    const currentDate = new Date(data.weatherData.current.dt * 1000)
+    dayjs.extend(LocalizedFormat);
+
   return (
     <>
     <div class="mx-auto p-4 bg-slate-300 h-screen flex justify-center">
@@ -20,25 +34,27 @@ export default function Index() {
                 <div class="px-6 py-6 relative">
                     <div class="flex mb-4 justify-between items-center">
                         <div>
-                            <h5 class="mb-0 font-medium text-xl">Anywhere, USA</h5>
-                            <h6 class="mb-0">April 04 2021</h6><small>Cloudy</small>
+                            <h5 class="mb-0 font-medium text-xl">{`${data.location.local_names.en}, ${data.location.state}`}</h5>
+                            <h6 class="mb-0">{dayjs(currentDate).format('LL')}</h6>
+                            <small>{data.weatherData.current.weather[0].main}</small>
+                            <p><small>{data.weatherData.current.weather[0].description}</small></p>
                         </div>
                         <div class="text-right">
-                            <h3 class="font-bold text-4xl mb-0"><span>39&deg;</span></h3>
+                            <h3 class="font-bold text-4xl mb-0"><span>{Math.round(data.weatherData.current.temp)}&deg;</span></h3>
                         </div>
                     </div>
                     <div class="block sm:flex justify-between items-center flex-wrap">
                         <div class="w-full sm:w-1/2">
-                            <div class="flex mb-2 justify-between items-center"><span>Temp</span><small class="px-2 inline-block">39.11&nbsp;&deg;</small></div>
+                            <div class="flex mb-2 justify-between items-center"><span>Temp</span><small class="px-2 inline-block">{Math.round(data.weatherData.current.temp)}&nbsp;&deg;</small></div>
                         </div>
                         <div class="w-full sm:w-1/2">
-                            <div class="flex mb-2 justify-between items-center"><span>Feels like</span><small class="px-2 inline-block">33.33&nbsp;&deg;</small></div>
+                            <div class="flex mb-2 justify-between items-center"><span>Feels like</span><small class="px-2 inline-block">{Math.round(data.weatherData.current.feels_like)}&nbsp;&deg;</small></div>
                         </div>
                         <div class="w-full sm:w-1/2">
-                            <div class="flex mb-2 justify-between items-center"><span>Temp min</span><small class="px-2 inline-block">24.9&nbsp;&deg;</small></div>
+                            <div class="flex mb-2 justify-between items-center"><span>Humidity</span><small class="px-2 inline-block">{Math.round(data.weatherData.current.humidity)} %</small></div>
                         </div>
                         <div class="w-full sm:w-1/2">
-                            <div class="flex mb-2 justify-between items-center"><span>Temp max</span><small class="px-2 inline-block">39&nbsp;&deg;</small></div>
+                            <div class="flex mb-2 justify-between items-center"><span>Dew Point</span><small class="px-2 inline-block">{Math.round(data.weatherData.current.dew_point)}&nbsp;&deg;</small></div>
                         </div>
                     </div>
                 </div>
