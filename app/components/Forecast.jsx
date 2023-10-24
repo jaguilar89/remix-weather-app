3e7acaf2ed7462abd2c44ef8ced50127903358c5
Jose Aguilar <jaguilarny@gmail.com>
2023-10-24 17:58:23 -0400

    adjust spacing

--- app/components/Forecast.jsx
+++ app/components/Forecast.jsx
@@ -2,12 +2,13 @@ import dayjs from "dayjs"
 
 export default function Forecast({ data }) {
     const days = data.weatherData.daily.map((day, index) => {
-        if (index === 0) return null
+        if (index === 0) return null // start at index 1 to exclude the current day
+        
         return (
             <div class="text-center mb-0 flex items-center justify-center flex-col">
                 <span class="block my-1">{dayjs(day.dt * 1000).format('ddd')}</span>
                 <span class="block my-1">{dayjs(day.dt * 1000).format('MMM DD')}</span>
-                <img src="https://i.imgur.com/ffgW9JQ.png" class="block w-8 h-8" />
+                <img src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`} class="block w-8 h-8" />
                 <span class="block my-1">{Math.round(day.temp.day)}&deg;</span>
             </div>
         )
@@ -15,12 +16,13 @@ export default function Forecast({ data }) {
 
     return (
         <div class="px-6 py-6 relative">
-                    <div class="text-center justify-between items-center flex" style={{flexFlow: 'initial'}}>
+                    <div class="text-center justify-between items-center flex space-x-10" style={{flexFlow: 'initial'}}>
                         
                         {days}
 
                     </div>
-                </div>
+        </div>
+
     )
 }
 
@@ -41,3 +43,4 @@ MMM = Abbreviated month name
 DD  = Day of the month, 2 digits
 ddd = The short name of the day of the week
 */
+
